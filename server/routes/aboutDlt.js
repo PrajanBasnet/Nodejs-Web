4672633e48bea9e40fad04788abeb857c47c8acc
PrajanBasnet <spiderbasnet140@gmail.com>
2024-11-28 00:37:46 +0545

    edit done

--- server/routes/aboutDlt.js
+++ server/routes/aboutDlt.js
@@ -12,4 +12,30 @@ router.get('/about',async(req,res)=>{
      return res.redirect('/about');
   })
 
+
+  router.post('/update',async(req,res)=>{
+   let id = req.body.id;
+
+   console.log(id);
+   let data =  await Blog.findById({_id: id});
+   console.log(data.title);
+    res.render('edit',{data});
+})
+
+router.post('/updateReal',async(req,res)=>{
+   let id = req.body.id;
+   const filter = {
+      _id : id
+   }
+   const update = {
+      "title":req.body.title,
+      "author":req.body.author,
+      "content":req.body.content
+   };
+
+ await Blog.findOneAndUpdate(filter,update);
+   return res.redirect('about');
+
+})
+
   module.exports = router;
